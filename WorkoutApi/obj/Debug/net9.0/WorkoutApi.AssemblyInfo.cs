994893daa878827c949e89962851ccc7d6ad2e5c
Sampeezy <createdbysamp@gmail.com>
2025-09-22 23:54:39 -0500

    added solution file

--- WorkoutApi/obj/Debug/net9.0/WorkoutApi.AssemblyInfo.cs
+++ WorkoutApi/obj/Debug/net9.0/WorkoutApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkoutApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6585f36158a718de1eac79512ff8731f96e4611")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63dc161a3387af848a0c28801081ddc5599a3fd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkoutApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkoutApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
