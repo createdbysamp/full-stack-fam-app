63dc161a3387af848a0c28801081ddc5599a3fd7
Sampeezy <createdbysamp@gmail.com>
2025-09-22 23:51:45 -0500

    Remove build artifacts from tracking and update .gitignore

--- WorkoutApi/obj/Debug/net9.0/WorkoutApi.AssemblyInfo.cs
+++ WorkoutApi/obj/Debug/net9.0/WorkoutApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkoutApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59c07bf72fbc384470d61b5366cd6f87f7817619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6585f36158a718de1eac79512ff8731f96e4611")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkoutApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkoutApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
