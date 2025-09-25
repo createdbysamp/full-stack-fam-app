4a1e2e5fa71df5d2d08299e558a1682e425cee04
Ursula Deese <u.deese@yahoo.com>
2025-09-25 13:51:30 -0500

    home page img tweak

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
             <div className="flex-fill d-flex flex-column justify-content-center align-items-center text-center text-white">
                 <h1 className="fw-bold display-4">Welcome to Swole Buddy!</h1>
                 <p className="lead">A performative AI app that generates you a customized workout workout.</p>
-                <img src="/images/hero-dumbbells.jpg" alt="dumbbells" className="img-fluid rounded w-75" />
+                <img src="/images/hero-dumbbells.jpg" alt="dumbbells" className="img-fluid rounded w-50" />
                 <Link to="/login" className="btn btn-lg btn-light mt-3">Get Started</Link>
             </div>
         </div>
