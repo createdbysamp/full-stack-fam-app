ed00c65715611ad6a7f02c9790656bd7e7b84b78
iqunlim <mrfoobar2000@gmail.com>
2025-09-26 09:50:12 -0500

    Add Swagger UI

--- WorkoutApi/Program.cs
+++ WorkoutApi/Program.cs
@@ -109,11 +109,17 @@ builder
 // generic services
 builder.Services.AddAuthorization();
 
+// swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
